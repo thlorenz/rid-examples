2e07f97e1c8dcda838df867fd2f02c7de51f2aca
Thorsten Lorenz <thlorenz@gmx.de>
2021-05-11 23:14:21 -0600

    dart: adding todo app

---
+++ dart/todo/README.md
@@ -0,0 +1,86 @@
+# Rid Todo Example
+
+An example todo app with user interaction implemented in Dart and app logic in Rust.
+
+## Getting Started
+
+_Please see [Caveats](#Caveats) first_.
+
+```sh
+./sh/build
+./sh/run
+```
+
+Repeat the first step every time you modify Rust code.
+
+## Rid Model 
+
+What the Model annotations communicate to _rid_:
+
+```rust
+#[rid::model]                 // this is a model and all its fields should be accessible from Dart
+#[rid::structs(Todo)]         // the referenced Todo type is a struct
+#[rid::enums(Filter)]         // the referenced Filter type is an enum
+#[derive(Debug, rid::Debug)]  // expose a `model.debug(pretty?)` function to Dart 
+pub struct Model {
+    last_added_id: u32,
+    todos: Vec<Todo>,
+    filter: Filter,
+}
+```
+
+Possible Model use in Dart code:
+
+```dart
+final filter = model.filter;
+for (final todo in model.todos.iter()) {
+  // do something
+}
+```
+
+## Rid Message
+
+How to setup sending messages to the _Model_ via _rid_:
+
+```rust
+#[rid::message(Model)]    // this is a message that will update the model
+#[rid::enums(Filter)]     // the referenced Filter type is an enum
+pub enum Msg {
+    AddTodo(String),      // sent via Dart: model.msgAddTodo("Learn Rid");
+
+    ToggleTodo(u32),      // sent via Dart: model.msgToggleTodo(todoId);
+    CompleteAll,          // sent via Dart: model.msgCompleteAll();
+
+    SetFilter(Filter),    // sent via Dart: model.msgSetFilter(RidFilter.index);
+}
+
+impl Model {
+    fn update(&mut self, msg: Msg) {
+      // handle the message here
+    }
+}
+```
+
+## Exporting Methods
+
+```rust
+#[rid::export]                      // rid will scan this impl block for exports
+impl Model {
+    #[rid::export(initModel)]       // exports this method to be called via Dart: rid_ffi.initModel(); 
+    fn new() -> Self {
+        Self {
+            last_added_id: 0,
+            todos: vec![],
+            filter: Filter::All,
+        }
+    }
+}
+```
+
+## Caveats
+
+At this point _Rid_ hasn't been published, therefore the build step cannot be performed and
+this example only serves to demonstrate what is possible once it _is_ published and open
+sourced.
+
+For more information please see [_Is Rid Open Sourced?_](../../README.md#is-rid-open-sourced)
---
+++ dart/todo/lib/main.dart
@@ -0,0 +1,115 @@
+import 'generated/rid_generated.dart';
+import 'dart:io';
+
+printStatus(Pointer<Model> model) {
+  final todos = model.todos;
+  final total = todos.length;
+
+  final filter = model.filter;
+  final matchingTodos = model.filtered_todos();
+
+  print("Total Todos:     $total");
+  print("Filter:          ${filter.display()}");
+  print("\nMatching Todos:");
+  for (final todo in matchingTodos.iter()) {
+    print("    ${todo.display()}");
+  }
+  matchingTodos.dispose();
+}
+
+bool handleCommand(Pointer<Model> model, String line) {
+  String cmd;
+  String payload;
+
+  if (line.length > 2) {
+    cmd = line.substring(0, 3);
+    payload = line.substring(3).trim();
+  } else {
+    cmd = line.substring(0, 2);
+    payload = "";
+  }
+
+  switch (cmd) {
+    case "add":
+      model.msgAddTodo(payload);
+      break;
+    case "del":
+      model.msgRemoveTodo(int.parse(payload));
+      break;
+    case "cmp":
+      model.msgCompleteTodo(int.parse(payload));
+      break;
+    case "tog":
+      model.msgToggleTodo(int.parse(payload));
+      break;
+    case "rst":
+      model.msgRestartTodo(int.parse(payload));
+      break;
+    case "fil":
+      final filter = payload == "cmp"
+          ? RidFilter.Completed
+          : payload == "pen"
+              ? RidFilter.Pending
+              : RidFilter.All;
+      model.msgSetFilter(filter.index);
+      break;
+    case "ca":
+      model.msgCompleteAll();
+      break;
+    case "dc":
+      model.msgRemoveCompleted();
+      break;
+    case "ra":
+      model.msgRestartAll();
+      break;
+
+    default:
+      print("\nUnknown command '$cmd'\n");
+      return false;
+  }
+  return true;
+}
+
+printCommands() {
+  print("\nPlease select one of the below:\n");
+  print("  add <todo title>  -- to add a todo");
+  print("  del <todo id>     -- to delete a todo by id");
+  print("  cmp <todo id>     -- to complete a todo by id");
+  print("  rst <todo id>     -- to restart a todo by id");
+  print("  tog <todo id>     -- to toggle a todo by id");
+  print("  fil all|cmp|pen   -- to set filter to");
+  print("  ca                -- to completed all todos");
+  print("  dc                -- to delete completed todos");
+  print("  ra                -- to restart all todos");
+  print("  q                 -- to quit");
+}
+
+void main(List<String> args) {
+  final model = rid_ffi.initModel();
+  {
+    model.msgAddTodo("Complete this Todo via:     cmp 1");
+    model.msgAddTodo("Delete this Todo via:       del 2");
+    model.msgAddTodo("Toggle this Todo via:       tog 3");
+    model.msgAddTodo("Restart the first Todo via: rst 1");
+
+    String? input;
+    bool ok = true;
+
+    while (true) {
+      if (ok) {
+        print("\x1B[2J\x1B[0;0H");
+      }
+      printStatus(model);
+      printCommands();
+      stdout.write("\n> ");
+      input = stdin.readLineSync();
+      if (input == "q") {
+        break;
+      }
+      if (input != null && input.length > 1) {
+        ok = handleCommand(model, input.trim());
+      }
+    }
+  }
+  model.dispose();
+}
