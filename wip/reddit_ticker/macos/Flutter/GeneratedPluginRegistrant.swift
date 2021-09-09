699d57885643e98407bc40320f1e353cca0d5ab8
Thorsten Lorenz <thlorenz@gmx.de>
2021-09-08 21:02:04 -0500

    reddit: adding url launcher and giving permissions for https

--- wip/reddit_ticker/macos/Flutter/GeneratedPluginRegistrant.swift
+++ wip/reddit_ticker/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import plugin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   Plugin.register(with: registry.registrar(forPlugin: "Plugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
