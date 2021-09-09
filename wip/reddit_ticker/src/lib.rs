0c4b8ca5d80305e0f3656453c1ff3c918980e709
Thorsten Lorenz <thlorenz@gmx.de>
2021-09-08 20:51:02 -0500

    reddit: adding shared URLs by cutting off query string

--- wip/reddit_ticker/src/lib.rs
+++ wip/reddit_ticker/src/lib.rs
@@ -199,6 +199,11 @@ struct Page {
 }
 
 fn query_page(url: &str) -> Result<Page> {
+    // Cut off query string
+    let url = match url.find('?') {
+        Some(idx) => &url[..idx],
+        None => url,
+    };
     let url = format!("{}.json", url.trim_end_matches("/"));
 
     let page_response: PageRoot = ureq::get(&url)
