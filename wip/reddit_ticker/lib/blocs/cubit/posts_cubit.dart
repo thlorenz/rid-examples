a4d9ee243a3839c3789d10021b63c0d82b51646b
Thorsten Lorenz <thlorenz@gmx.de>
2021-09-08 20:57:04 -0500

    reddit: refreshing posts state when one is removed

--- wip/reddit_ticker/lib/blocs/cubit/posts_cubit.dart
+++ wip/reddit_ticker/lib/blocs/cubit/posts_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
@@ -7,8 +9,17 @@ part 'posts_state.dart';
 
 class PostsCubit extends Cubit<PostsState> {
   final _store = Store.instance;
+  late final StreamSubscription<PostedReply> removedPostsSub;
+
   PostsCubit() : super(PostsState([])) {
+    _subscribe();
     refresh();
+  }
+
+  void _subscribe() {
+    removedPostsSub = replyChannel.stream
+        .where((x) => x.type == Reply.StoppedWatching)
+        .listen((_) => refresh());
   }
 
   void refresh() {
