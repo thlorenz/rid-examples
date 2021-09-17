5e04d0aacae4770da624d8bc123c8fa2e230ea0a
Thorsten Lorenz <thlorenz@gmx.de>
2021-09-17 18:30:44 -0500

    rt: reddit models and deps

--- flutter/reddit_ticker/src/lib.rs
+++ flutter/reddit_ticker/src/lib.rs
@@ -1,5 +1,1 @@
-#[rid::export]
-pub fn hello_world(id: u8) -> String {
-    rid::log_info!("Providing hello world for {}", id);
-    "hello world".to_string()
-}
+mod reddit;
---
+++ flutter/reddit_ticker/src/reddit/mod.rs
@@ -0,0 +1,5 @@
+mod reddit_api_response;
+mod reddit_page_response;
+
+pub use reddit_api_response::*;
+pub use reddit_page_response::*;
