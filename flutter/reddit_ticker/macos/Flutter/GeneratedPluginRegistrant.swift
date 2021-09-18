8c63ed175f96d44d04c24219ffd94d0d7490f56d
Thorsten Lorenz <thlorenz@gmx.de>
2021-09-18 17:20:38 -0500

    rt: install path_provider

--- flutter/reddit_ticker/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/reddit_ticker/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_macos
 import plugin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   Plugin.register(with: registry.registrar(forPlugin: "Plugin"))
 }
